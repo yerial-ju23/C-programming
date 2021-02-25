9cac362ab7fc30dee045ffbed8f82677156c69d0
김연주 <71300495+Yeonju23@users.noreply.github.com>
2021-02-25 19:54:20 +0900

    Create 1004.c

---
+++ Codeup/1004.c
@@ -0,0 +1,11 @@
+//작은 따옴표(single quotation mark)가 들어있는 특수한 형태의 출력문에 대한 연습
+//printf( ) 함수로 따옴표 문자 ' 를 출력하기 위해서는 \' 로 입력
+// \' 는 작은따옴표 문자 자체 출력
+// \" 는 큰따옴표 문자 자체 출력
+
+#include <stdio.h>
+
+int main(coid){
+    printf("\'Hello\'");
+    return 0;
+}
